05e77b8404644e66b4905c1879057a437c5a546b
Felipe Neves <felipelabs@gmail.com>
2024-07-01 20:41:03 -0300

    Create domain

---
+++ src/main/java/com/neves/notificacao/domain/Usuario.java
@@ -0,0 +1,16 @@
+package com.neves.notificacao.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private Double renda;
+}
