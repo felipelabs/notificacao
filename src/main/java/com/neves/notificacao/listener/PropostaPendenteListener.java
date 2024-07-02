31aa836ec327a55bd822a333c16b60c956e40411
Felipe Neves <felipelabs@gmail.com>
2024-07-01 21:15:41 -0300

    Add configuration aws, rabbitmq and service

--- src/main/java/com/neves/notificacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/neves/notificacao/listener/PropostaPendenteListener.java
@@ -1,13 +1,19 @@
 package com.neves.notificacao.listener;
 
+import com.neves.notificacao.constante.MensagemConstante;
+import com.neves.notificacao.domain.Proposta;
+import com.neves.notificacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
-    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente(){
+    NotificacaoSnsService notificacaoSnsService;
 
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta){
+        String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+        notificacaoSnsService.notificar(proposta.getUsuario().getTelefone(), mensagem);
     }
 }
